1f2a339f5c69aeb0fe116c6d1d151646998ac2c2
Dennis Chacko <dennis.chacko@amwell.com>
2020-10-31 17:12:41 -0400

    initial commit

---
+++ src/main/java/com/todaystech/SpringConfigService/SpringConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package com.todaystech.SpringConfigService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServiceApplication.class, args);
+	}
+
+}
